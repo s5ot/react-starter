db58dda61fb1b2d14d365da348c6f0000bf1f06b
sogo.ohta <sogo.ohta@gmail.com>
2015-01-16 15:15:15 +0900

    Enable to sort

--- app/Application/index.jsx
+++ app/Application/index.jsx
@@ -35,11 +35,14 @@ var Application = React.createClass({
   mixins: [StateFromStoreMixin],
 
   getInitialState: function() {
-    return {data: [
-      {'name': 'Nexus S', 'snippet': 'Fast just got faster with Nexus S.'},
-      {'name': 'Motorola XOOM™ with Wi-Fi', 'snippet': 'The Next, Next Generation tablet.'},
-      {'name': 'MOTOROLA XOOM™', 'snippet': 'The Next, Next Generation tablet.'}
-    ]};
+    return {
+      data: [
+        {'name': 'Nexus S', 'snippet': 'Fast just got faster with Nexus S.', 'age': 1},
+        {'name': 'Motorola XOOM™ with Wi-Fi', 'snippet': 'The Next, Next Generation tablet.', 'age': 2},
+        {'name': 'MOTOROLA XOOM™', 'snippet': 'The Next, Next Generation tablet.', 'age': 3}
+      ],
+      orderProp: 'age'
+    };
   },
 
   componentDidMount: function() {
@@ -47,34 +50,67 @@ var Application = React.createClass({
     var newData = [];
     var search = this.refs.search.getDOMNode();
     var keyups = Rx.Observable.fromEvent(search,  'keyup')
-    .map(function (e) {
-      return e.target.value;
-    })
-    .throttle(500)
-    .distinctUntilChanged()
-    .map(
-      function(x) {
-        newData = [];
-        this.setState({data: newData});
-        return x;
-      }.bind(this)
-    )
-    .flatMap(function(s) {
-      var matcher = new RegExp(".*" + s + ".*", 'i');
-      return Rx.Observable.fromArray(data).filter(function(phone) { return phone.name.match(matcher) || phone.snippet.match(matcher) });
-    })
-    .subscribe(
-      function(x) {
-        newData.push(x);
-        this.setState({data: newData});
-      }.bind(this),
-      function(err) {
-        console.log('error');
-      },
-      function() {
-        console.log('completed');
-      }
-    );
+      .map(function (e) {
+        return e.target.value;
+      })
+      .throttle(500)
+      .distinctUntilChanged()
+      .map(
+        function(x) {
+          newData = [];
+          this.setState({data: newData});
+          return x;
+        }.bind(this)
+      )
+      .flatMap(function(s) {
+        var matcher = new RegExp(".*" + s + ".*", 'i');
+        return Rx.Observable.fromArray(data).filter(function(phone) { return phone.name.match(matcher) || phone.snippet.match(matcher) });
+      })
+      .subscribe(
+        function(x) {
+          newData.push(x);
+          this.setState({data: newData});
+        }.bind(this),
+        function(err) {
+          console.log('error');
+        },
+        function() {
+          console.log('completed');
+        }
+      );
+
+    var sort = this.refs.sort.getDOMNode();
+    var changes = Rx.Observable.fromEvent(sort, 'change')
+      .map(
+        function (e) {
+          this.setState({orderProp: e.target.value});
+          return e.target.value;
+        }.bind(this)
+      )
+      .subscribe(
+        function(x) {
+          console.log(x);
+          console.log(this.state.orderProp);
+          console.log(this.state.data);
+          console.log(this.state.data[0][this.state.orderProp]);
+          var orderProp = this.state.orderProp;
+          this.setState({data: this.state.data.sort(function(a, b) {
+            if (a[orderProp] > b[orderProp]) {
+              return 1;
+            }
+            if (a[orderProp] < b[orderProp]) {
+              return -1;
+            }
+            return 0;
+          })});
+        }.bind(this),
+        function(err) {
+          console.log('error');
+        },
+        function() {
+          console.log('completed');
+        }
+      );
  },
 
   statics: {
@@ -90,7 +126,14 @@ var Application = React.createClass({
     return <div className={this.state.loading ? "application loading" : "application"}>
       {this.state.loading ? <div style={{float: "right"}}>loading...</div> : null}
       <h1>react-starter</h1>
-      <input type="text" ref="search" />
+      <div>
+      <label>Search: </label><input type="text" ref="search" />
+      </div>
+      Sort by:
+      <select ref="sort" value={this.state.orderProp}>
+      <option value="name">Alphabetical</option>
+      <option value="age">Newest</option>
+      </select>
       <PhoneList data={this.state.data} />
     </div>;
   },
